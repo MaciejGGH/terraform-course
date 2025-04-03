8364458bc13b8bd2de90f63e178705ebc002e777
Maciej Grabowski <maciejg@gmail.com>
2025-04-03 17:46:44 +0200

    tags

--- solutions/proj03-import-lambda/provider.tf
+++ solutions/proj03-import-lambda/provider.tf
@@ -8,11 +8,16 @@ terraform {
     archive = {
       source  = "hashicorp/archive"
       version = "~> 2.0"
-
     }
   }
 }
 
 provider "aws" {
   region = "eu-west-1"
+  default_tags {
+    tags = {
+      ManagedBy = "Terraform"
+      Project   = "project03-import-lambda"
+    }
+  }
 }
